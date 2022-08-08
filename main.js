d8fe278ecab408ad78f665086e1afe3eeb1d1c66
Harold Bengter <Haroldbe55@yahoo.com>
2022-08-08 13:02:09 -0400

    Finished all exercise todos

---
+++ main.js
@@ -0,0 +1,31 @@
+//Exercicse 1 and 2
+const firstName = "Harold"
+let lastName = "Bengter"
+var age = 24;
+console.log(`Hi my name is ${firstName} ${lastName}. I am ${age}`);
+
+//Exercise 3
+let language = "javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`${language} was created in ${createdYear} You might be wondering is it case sensitive? That is ${isCaseSensitive}`);
+
+//Exercise 4
+let price = 19.99;
+let isOnSales = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let SelectedSize = "M";
+
+console.log(`The price of this shorts is ${price}. It is on sale by ${salePercentage} percent! Their are ${stock} left in stock.`);
+
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`My favorite book is ${title} by ${author}. It has a total of ${pageCount} and i'm currently on page ${bookmark}.`);
+
